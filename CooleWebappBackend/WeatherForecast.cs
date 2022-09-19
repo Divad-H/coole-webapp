8a13676b93fda9d6c6b2c0be88e5a22c15f7a365
David Hübscher <David.Huebscher@procom.de>
2022-09-19 17:48:09 +0200

    WeatherForecast: make required fields required.

--- CooleWebappBackend/WeatherForecast.cs
+++ CooleWebappBackend/WeatherForecast.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CooleWebappBackend
 {
   public class WeatherForecast
   {
+    [Required]
     public DateTime Date { get; set; }
 
+    [Required]
     public int TemperatureC { get; set; }
 
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
