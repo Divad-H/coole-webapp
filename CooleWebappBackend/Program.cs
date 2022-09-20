8cf7b8b7510d31e90de07e42591c414f96f78e0f
David Hübscher <David.Huebscher@procom.de>
2022-09-20 11:00:09 +0200

    Configure the database from main and create an initial migration.

--- CooleWebappBackend/Program.cs
+++ CooleWebappBackend/Program.cs
@@ -1,3 +1,5 @@
+using Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument();
+builder.Services.AddCooleWebappDatabase(builder.Configuration);
 
 var app = builder.Build();
+await DbSetup.InitializeCooleWebappDatabase(app.Services, app.Lifetime.ApplicationStopping);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ README.md
@@ -0,0 +1,7 @@
+# Coole Webapp
+
+### Creating Migrations
+
+#### Create Initial Migrations
+
+From within the database project directory call `dotnet ef migrations add InitialCreate --startup-project ..\CooleWebappBackend\CooleWebappBackend.csproj`
