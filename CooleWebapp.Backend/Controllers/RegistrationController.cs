2de384cf69fede8edfa2a9ccde76dfac72c2c98d
David Hübscher <huebschersdavid@gmail.com>
2022-10-19 20:35:49 +0200

    Enable api-documentation.

--- CooleWebapp.Backend/Controllers/RegistrationController.cs
+++ CooleWebapp.Backend/Controllers/RegistrationController.cs
@@ -13,9 +13,16 @@ public class RegistrationController : ControllerBase
     _userRegistration = userRegistration;
   }
 
+  /// <summary>
+  /// Start the registration of a new user
+  /// </summary>
+  /// <param name="registrationData">Information required for the registration</param>
+  /// <param name="ct">Allows aborting the operation</param>
+  /// <returns>The task of the operation</returns>
+  [Route("register")]
   [HttpPost]
-  public async Task RegisterUser(RegistrationData registrationData, CancellationToken ct)
+  public Task RegisterUser(RegistrationData registrationData, CancellationToken ct)
   {
-    await _userRegistration.RegisterUser(registrationData, ct);
+    return _userRegistration.RegisterUser(registrationData, ct);
   }
 }
--- CooleWebapp.Entities/Entities/CoolUser.cs
+++ CooleWebapp.Entities/Entities/CoolUser.cs
@@ -1,11 +1,29 @@
 ﻿namespace CooleWebapp.Core.Entities
 {
+  /// <summary>
+  /// Application user data
+  /// </summary>
   public record CoolUser
   {
+    /// <summary>
+    /// Unique id that identifies the user
+    /// </summary>
     public ulong Id { get; set; }
+    /// <summary>
+    /// The id of the related WebappUser that contains login information
+    /// </summary>
     public string WebappUserId { get; set; } = string.Empty;
+    /// <summary>
+    /// The name of the user
+    /// </summary>
     public string Name { get; set; } = string.Empty;
+    /// <summary>
+    /// The title of the user (e.g. Lord)
+    /// </summary>
     public string? Title { get; set; }
+    /// <summary>
+    /// The three letter initials of the user
+    /// </summary>
     public string Initials { get; set; } = string.Empty;
   }
 }
