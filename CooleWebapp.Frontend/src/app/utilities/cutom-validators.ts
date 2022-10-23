9a059d37f492fd9e85ff7138273e19c04ea25d1e
David Hübscher <huebschersdavid@gmail.com>
2022-10-23 23:51:42 +0200

    Add validators and error state matchers for registration ui.

---
+++ CooleWebapp.Frontend/src/app/utilities/cutom-validators.ts
@@ -0,0 +1,48 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+
+export namespace CustomValidators {
+  
+  export function patternValidator(regex: RegExp, error: ValidationErrors): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!control.value) {
+        return null;
+      }
+      return regex.test(control.value) ? null : error;
+    };
+  }
+  
+  export const containsNumber: ValidatorFn =
+    patternValidator(/\d/, { containsNumber: true });
+
+  export const containsSpecialCharacter: ValidatorFn =
+    patternValidator(/[^a-zA-Z0-9]/, {
+      containsSpecialCharacter: true,
+    });
+
+  export function containsUppercase(control: AbstractControl): ValidationErrors | null {
+    if (!control.value) {
+      return null;
+    }
+    return control.value.toLowerCase() !== control.value
+      ? null
+      : { containsUppercase: true };
+  }
+
+  export function containsLowercase(control: AbstractControl): ValidationErrors | null {
+    if (!control.value) {
+      return null;
+    }
+    return control.value.toUpperCase() !== control.value
+      ? null
+      : { containsLowercase: true };
+  }
+
+  export function containsNoWhitespace(control: AbstractControl): ValidationErrors | null {
+    if (!control.value) {
+      return null;
+    }
+    return new RegExp("\\s+").test(control.value)
+      ? { containsNoWhitespace: true }
+      : null;
+  }
+}
