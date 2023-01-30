eb55d62f28790e242662316609d65c1cc9be0953
David Hübscher <huebschersdavid@gmail.com>
2023-01-30 01:24:21 +0100

    Call the buy products endpoint from the buy dialog.

--- CooleWebapp.Frontend/src/app/home/home.module.ts
+++ CooleWebapp.Frontend/src/app/home/home.module.ts
@@ -8,6 +8,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSelectModule } from '@angular/material/select';
@@ -59,6 +60,7 @@ import { BuyDialog } from "./shop/buy-dialog/buy-dialog.component";
     MatInputModule,
     MatListModule,
     MatPaginatorModule,
+    MatProgressBarModule,
     MatProgressSpinnerModule,
     MatSelectModule,
     MatSidenavModule,
--- CooleWebapp.Frontend/src/app/home/shop/buy-dialog/buy-dialog.component.ts
+++ CooleWebapp.Frontend/src/app/home/shop/buy-dialog/buy-dialog.component.ts
@@ -1,7 +1,8 @@
-import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { map, Observable, startWith } from 'rxjs';
+import { BehaviorSubject, catchError, tap, map, mapTo, Observable, of, startWith, Subject, Subscription, switchMap } from 'rxjs';
+import { CooleWebappApi } from '../../../../generated/coole-webapp-api';
 import { Product } from '../shop.component';
 
 export interface DialogData {
@@ -13,15 +14,19 @@ export interface DialogData {
   styleUrls: ['./buy-dialog.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class BuyDialog {
+export class BuyDialog implements OnInit, OnDestroy {
 
+  private readonly subscriptions = new Subscription();
   readonly form: FormGroup;
   readonly total: Observable<number | string>;
+  private readonly buySubject = new Subject();
+  public readonly busy = new BehaviorSubject(false);
 
   constructor(
     public readonly dialogRef: MatDialogRef<BuyDialog>,
     @Inject(MAT_DIALOG_DATA) public readonly data: DialogData,
-    private readonly formBuilder: FormBuilder
+    private readonly formBuilder: FormBuilder,
+    private readonly shopClient: CooleWebappApi.ShopClient,
   ) {
     this.form = formBuilder.group({
       amount: [1, [Validators.required, Validators.min(1), Validators.pattern('[0-9]*')]]
@@ -32,13 +37,41 @@ export class BuyDialog {
     );
   }
 
+  ngOnInit(): void {
+    this.subscriptions.add(
+      this.buySubject.pipe(
+        tap(() => this.busy.next(true)),
+        switchMap(() => 
+          this.shopClient.buyProducts(new CooleWebappApi.BuyProductsRequestModel({
+            products: [ new CooleWebappApi.ProductAmount({
+              amount: this.form.value.amount,
+              expectedPrice: this.form.value.amount * this.data.product.price,
+              productId: this.data.product.id
+            })]
+          })).pipe(
+            mapTo(true),
+            catchError(err => { console.log(err); return of(false) })
+          ))
+      ).subscribe(success => {
+        this.busy.next(false);
+        if (success) {
+          this.dialogRef.close(this.form.value.amount);
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
   onNoClick(): void {
     this.dialogRef.close();
   }
 
   onBuyClick(): void {
     if (this.form.valid) {
-      this.dialogRef.close(this.form.value.amount);
+      this.buySubject.next({});
     }
   }
 }
