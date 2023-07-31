63fec61bf35767904ef94d121ef99491d5cef044
David Hübscher <huebschersdavid@gmail.com>
2023-07-31 02:14:16 +0200

    Add OrderItems to the DbContext and add a migration to rename the table.

--- CooleWebapp.Database/Migrations/20230730235811_AddOrderItems.Designer.cs
+++ CooleWebapp.Database/Migrations/20230730235811_AddOrderItems.Designer.cs
@@ -4,6 +4,7 @@ using CooleWebapp.Database.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CooleWebapp.Database.Migrations
 {
     [DbContext(typeof(WebappDbContext))]
-    partial class WebappDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230730235811_AddOrderItems")]
+    partial class AddOrderItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
